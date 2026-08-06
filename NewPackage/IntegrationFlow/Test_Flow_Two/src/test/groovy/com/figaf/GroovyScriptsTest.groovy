411a75ed501c365634537cd7390291665c914df6
IRT <dev@figaf.com>
2026-08-06 14:00:47 +0000

    Groovy script tests were updated for Test_Flow_Two iflow

--- NewPackage/IntegrationFlow/Test_Flow_Two/src/test/groovy/com/figaf/GroovyScriptsTest.groovy
+++ NewPackage/IntegrationFlow/Test_Flow_Two/src/test/groovy/com/figaf/GroovyScriptsTest.groovy
@@ -7,7 +7,8 @@ class GroovyScriptsTest extends AbstractGroovyTest {
 
     @ParameterizedTest
     @ValueSource(strings = [
-            "src/test/resources/test-data-files/script1/processData/test-data-1.json"
+            "src/test/resources/test-data-files/script1/processData/test-data-1.json",
+            "src/test/resources/test-data-files/script1/processData/test-data-2.json"
     ])
     void test_script1Groovy(String testDataFile) {
         String groovyScriptPath = "src/main/resources/script/script1.groovy"
@@ -16,7 +17,8 @@ class GroovyScriptsTest extends AbstractGroovyTest {
 
     @ParameterizedTest
     @ValueSource(strings = [
-            "src/test/resources/test-data-files/script2/processData/test-data-1.json"
+            "src/test/resources/test-data-files/script2/processData/test-data-1.json",
+            "src/test/resources/test-data-files/script2/processData/test-data-2.json"
     ])
     void test_script2Groovy(String testDataFile) {
         String groovyScriptPath = "src/main/resources/script/script2.groovy"
